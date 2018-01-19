ae5288519699749e7073e034ffd0e5190cea3f4c
Valery Zaytsev <35430941+kurinajagrudka@users.noreply.github.com>
2018-01-19 02:27:01 +0200

    Add files via upload

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,72 @@
+<?php return array (
+  'arrilot/laravel-widgets' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Arrilot\\Widgets\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Widget' => 'Arrilot\\Widgets\\Facade',
+      'AsyncWidget' => 'Arrilot\\Widgets\\AsyncFacade',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'graham-campbell/markdown' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'GrahamCampbell\\Markdown\\MarkdownServiceProvider',
+    ),
+  ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+  'larapack/voyager-hooks' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Larapack\\VoyagerHooks\\VoyagerHooksServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'laravelcollective/html' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Collective\\Html\\HtmlServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Form' => 'Collective\\Html\\FormFacade',
+      'Html' => 'Collective\\Html\\HtmlFacade',
+    ),
+  ),
+  'tcg/voyager' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'TCG\\Voyager\\VoyagerServiceProvider',
+    ),
+  ),
+);
